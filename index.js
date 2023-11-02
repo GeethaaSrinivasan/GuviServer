7e54f52f8da13f1a40209ba3b60d151b587e112e
GeethaaSrinivasan <89134180+GeethaaSrinivasan@users.noreply.github.com>
2023-11-02 19:04:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const db = require("./db");
 const cors = require("cors");
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 3004;
 app.use(cors());
 app.use(express.json());
 

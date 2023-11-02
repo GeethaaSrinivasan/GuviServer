552ae39fd5d3b26741667c48d3ba7ad7ecf6cbf8
GeethaaSrinivasan <89134180+GeethaaSrinivasan@users.noreply.github.com>
2023-11-02 19:11:06 +0530

    Update db.js
    added port

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const db = mysql.createConnection({
 host: "sql12.freesqldatabase.com",
 user: "sql12656888",
 password: "tIRbec3H7z",
-database:"sql12656888" 
+database:"sql12656888",
+port:3307
 })
 
 module.exports = db;
